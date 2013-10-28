029ca9f1cfa69a59d0ef8d95b909b93f2534f331
SebMadgwick <sebmadgwick@gmail.com>
2013-10-28 11:31:41 +0000

    Support ASCII packet mode

--- ArduIMU_Glove/Send.cpp
+++ ArduIMU_Glove/Send.cpp
@@ -31,6 +31,8 @@ extern AHRS ahrs;
 //------------------------------------------------------------------------------
 // Definitions
 
+#define BINARY_PACKETS
+
 typedef union {
     int intVal;
     struct {
@@ -47,6 +49,7 @@ void Send::flexSensorData() {
     int packetLength = 0;
     IntUnion intUnion;
     FlexSensors::read();    // read sensors before sending
+#ifdef BINARY_PACKETS
     packet[packetLength++] = 'F';
     intUnion.intVal= (int)FlexSensors::channel[0];
     packet[packetLength++] = intUnion.msb;
@@ -67,6 +70,24 @@ void Send::flexSensorData() {
     packet[packetLength++] = intUnion.msb;
     packet[packetLength++] = intUnion.lsb;
     packet[packetLength++] = calcChecksum(packet, packetLength);
+#else
+    packet[packetLength++] = 'F';
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, (int)FlexSensors::channel[0]);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, (int)FlexSensors::channel[1]);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, (int)FlexSensors::channel[2]);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, (int)FlexSensors::channel[3]);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, (int)FlexSensors::channel[4]);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, (int)FlexSensors::channel[5]);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, calcChecksum(packet, packetLength));
+    packet[packetLength++] = '\r';
+#endif
     Serial.write((uint8_t*)packet, packetLength);
 }
 
@@ -74,6 +95,8 @@ void Send::sensorData() {
     char packet[128];
     int packetLength = 0;
     IntUnion intUnion;
+    Serial.write((uint8_t*)packet, packetLength);
+#ifdef BINARY_PACKETS
     packet[packetLength++] = 'S';
     intUnion.intVal= Calibration::gyrX;
     packet[packetLength++] = intUnion.msb;
@@ -103,12 +126,37 @@ void Send::sensorData() {
     packet[packetLength++] = intUnion.msb;
     packet[packetLength++] = intUnion.lsb;
     packet[packetLength++] = calcChecksum(packet, packetLength);
+#else
+    packet[packetLength++] = 'S';
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, Calibration::gyrX);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, Calibration::gyrY);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, Calibration::gyrZ);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, Calibration::accX);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, Calibration::accY);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, Calibration::accZ);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, Calibration::magX);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, Calibration::magY);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, Calibration::magZ);
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, calcChecksum(packet, packetLength));
+    packet[packetLength++] = '\r';
+#endif
     Serial.write((uint8_t*)packet, packetLength);
 }
 
 void Send::quaternionData() {
     char packet[64];
     int packetLength = 0;
+#ifdef BINARY_PACKETS
     IntUnion intUnion;
     packet[packetLength++] = 'Q';
     intUnion.intVal= (int)(10000.0f * ahrs.q0);
@@ -124,6 +172,21 @@ void Send::quaternionData() {
     packet[packetLength++] = intUnion.msb;
     packet[packetLength++] = intUnion.lsb;
     packet[packetLength++] = calcChecksum(packet, packetLength);
+    packet[packetLength++] = intUnion.lsb;
+#else
+    packet[packetLength++] = 'Q';
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, (int)(10000.0f * ahrs.q0));
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, (int)(10000.0f * ahrs.q1));
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, (int)(10000.0f * ahrs.q2));
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, (int)(10000.0f * ahrs.q3));
+    packet[packetLength++] = ',';
+    IntValToChars(packet, &packetLength, calcChecksum(packet, packetLength));
+    packet[packetLength++] = '\r';
+#endif
     Serial.write((uint8_t*)packet, packetLength);
 }
 
@@ -137,6 +200,40 @@ void Send::miscData(const char* const charArray) {
     }
     packet[packetLength++] = calcChecksum(packet, packetLength);
     Serial.write((uint8_t*)packet, packetLength);
+}
+
+void Send::IntValToChars(char* const charArray, int* const index, int i) {
+    static const char asciiDigits[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', };
+    div_t n;
+    int print = 0;
+    if(i < 0) {
+        charArray[(*index)++] = '-';
+        i = -i;
+    }
+    if(i >= 10000) {
+        n = div(i, 10000);
+        charArray[(*index)++] = asciiDigits[n.quot];
+        i = n.rem;
+        print = 1;
+    }
+    if(i >= 1000 || print) {
+        n = div(i, 1000);
+        charArray[(*index)++] = asciiDigits[n.quot];
+        i = n.rem;
+        print = 1;
+    }
+    if(i >= 100 || print) {
+        n = div(i, 100);
+        charArray[(*index)++] = asciiDigits[n.quot];
+        i = n.rem;
+        print = 1;
+    }
+    if(i >= 10 || print) {
+        n = div(i, 10);
+        charArray[(*index)++] = asciiDigits[n.quot];
+        i = n.rem;
+    }
+    charArray[(*index)++] = asciiDigits[i];
 }
 
 char Send::calcChecksum(const char* const packet, const int packetLength) {
--- ArduIMU_Glove/Send.h
+++ ArduIMU_Glove/Send.h
@@ -16,6 +16,7 @@ class Send {
         static void quaternionData();
         static void miscData(const char* const charArray);
     private:
+        static void IntValToChars(char* const charArray, int* const index, int i);
         static char calcChecksum(const char* const packet, const int packetLength);
 };
 
